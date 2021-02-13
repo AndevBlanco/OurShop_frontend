31bb85ac53113a2a42b7c0602d416f9e844cfb81
GrimeBlade <diegoc25905@gmail.com>
2021-02-13 00:59:58 -0500

    Avances: Acabe el menu desplegable. Pendiente: Maquetar todo el home y definr los estilos.

--- os_frontend/src/app/app.module.ts
+++ os_frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { MenuComponent } from './pages/menu/menu.component';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -8,10 +9,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MenuComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  exports: [MenuComponent]
 })
 export class AppModule {}
